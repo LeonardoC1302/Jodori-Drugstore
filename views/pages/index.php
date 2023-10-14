aac4336c9d3a527d5d4a30bad6c89f0cc39baffd
Leonardo Céspedes <lecespedes@estudiantec.cr>
2023-10-13 21:09:35 -0600

    Home Page

--- views/pages/index.php
+++ views/pages/index.php
@@ -1,1 +1,50 @@
-<h1>Inicio</h1>
+<div class="publicidad">
+    <p class="flecha"> < </p>
+    <img src="/images/vitybell.png" alt="Imagen Publicidad">
+    <div class="contenido-publicidad">
+        <p>¡Cabello y unas uñas más fuertes que nunca!</p>
+        <span>La cápsula que cuida tu belleza desde adentro</span>
+    </div>
+    <p class="flecha"> > </p>
+</div>
+
+
+
+<h1>Productos Destacados</h1>
+<div class="container-info">
+    <div class="contenedor-anuncios">
+        <div class="anuncio">
+            <div class="imagen-anuncio">
+                <img src="/images/abrilar.jpg" alt="Imagen del Anuncio">
+            </div>
+            <div class="contenido-anuncio">
+                <p>Abrilar 100ml Jarabe Via Oral</p>
+                <p class="precio">₡5300 I.V.A.I</p>
+            </div>
+        </div> <!--.anuncio-->
+
+        <div class="anuncio">
+            <div class="imagen-anuncio">
+                <img src="/images/cataflam.jpg" alt="Imagen del Anuncio">
+            </div>
+            <div class="contenido-anuncio">
+                <p>Abrilar 100ml Jarabe Via Oral</p>
+                <p class="precio">₡5300 I.V.A.I</p>
+            </div>
+        </div> <!--.anuncio-->
+
+        <div class="anuncio">
+            <div class="imagen-anuncio">
+                <img src="/images/enantyum.jpg" alt="Imagen del Anuncio">
+            </div>
+            <div class="contenido-anuncio">
+                <p>Abrilar 100ml Jarabe Via Oral</p>
+                <p class="precio">₡5300 I.V.A.I</p>
+            </div>
+        </div> <!--.anuncio-->
+    </div>
+    <a href="productos" class="ver-todos">
+        <img src="/images/plus.png" alt="Plus Symbol" class="plus-symbol">
+        <span>Ver Todos</span>
+    </a>
+</div>
