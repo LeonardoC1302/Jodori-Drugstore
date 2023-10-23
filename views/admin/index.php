fe7b3e817468e2aa11d89f0c7eb6d03536954e63
Leonardo Céspedes <lecespedes@estudiantec.cr>
2023-10-23 10:21:26 -0600

    Admin Pages

--- views/admin/index.php
+++ views/admin/index.php
@@ -4,3 +4,55 @@
 </div>
 
 <h1>Productos</h1>
+<table class="products">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Imagen</th>
+            <th>Precio</th>
+            <th>Categoría</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <!-- Meterlo en un for -->
+        <tr>
+            <td>1</td>
+            <td>Abrilar 100ml Jarabe Via Oral</td>
+            <td><img src="/images/abrilar.jpg" alt="Table Image" class="table-image"></td>
+            <td>₡5.300</td>
+            <td>Asma y Alergias</td>
+            <td>
+                <form method="POST" class="w-100" action="/admin/eliminar">
+                    <input type="hidden" name="id" value="1">
+                    <input type="hidden" name="type" value="producto">
+                    <input type="submit" class="red-btn-block" value="Eliminar">
+                </form>
+                <a href="/admin/actualizar" class="blue-btn">Actualizar</a>
+            </td>
+        </tr>
+        <!-- Hasta acá -->
+
+        <!-- Meterlo en un for -->
+        <tr>
+            <td>2</td>
+            <td>Cataflam D. 46.5mg Tableta Vía Oral</td>
+            <td><img src="/images/cataflam.jpg" alt="Table Image" class="table-image"></td>
+            <td>₡1.055</td>
+            <td>Dolor e Inflamación</td>
+            <td>
+                <form method="POST" class="w-100" action="/admin/eliminar">
+                    <input type="hidden" name="id" value="2">
+                    <input type="hidden" name="type" value="producto">
+                    <input type="submit" class="red-btn-block" value="Eliminar">
+                </form>
+                <a href="/admin/actualizar" class="blue-btn">Actualizar</a>
+            </td>
+        </tr>
+        <!-- Hasta acá -->
+
+    </tbody>
+
+</table>
--- views/admin/reporte.php
+++ views/admin/reporte.php
@@ -1,1 +1,29 @@
-<h1>Reporte</h1>
+<div class="datos">
+    <div class="dato">
+        <p>Productos en Inventario <span>110</span></p>
+    </div>
+
+    <div class="dato">
+        <p>Clientes Registrados <span>250</span></p>
+    </div>
+
+    <div class="dato">
+        <p>Total de Ventas <span>175</span></p>
+    </div>
+
+    <div class="dato">
+        <p>Diferencia de Ventas <span>+10</span></p>
+    </div>
+
+    <div class="dato">
+        <p>Ganancias Totales <span>₡1.205.340</span></p>
+    </div>
+</div>
+
+<div class="reporte">
+    <h1 class="align-left">Resumen de Ventas/Ganancias</h1>
+    <div class="grafico">
+        <img src="/images/grafico.jpg" alt="Imagen Gráfico">
+        <a class="orange-btn" href="/admin">Volver al panel</a>
+    </div>
+</div>
