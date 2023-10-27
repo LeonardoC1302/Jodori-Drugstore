18df5867a286604cec5f34a10c6669de5867aede
Leonardo Céspedes <lecespedes@estudiantec.cr>
2023-10-26 19:55:04 -0600

    Account Fix

--- views/pages/cuenta.php
+++ views/pages/cuenta.php
@@ -1,23 +1,25 @@
 <div class="info-cuenta">
-    <!-- <div class="imagen-cuenta"></div> -->
-    <form class="form">
-        <div class="form__field">
-            <label for="name" class="form__label">Nombre Usuario</label>
-            <input class="form__input" type="text" placeholder="Nombre de usuario" id="name" name="name">
-        </div> <!-- /form__field -->        
-        <div class="form__field">
-            <label for="phone" class="form__label">Teléfono</label>
-            <input class="form__input" type="tel" placeholder="Teléfono de usuario" id="phone" name="phone">
-        </div> <!-- /form__field -->        
-        <div class="form__field">
-            <label for="email" class="form__label">Correo Electrónico</label>
-            <input class="form__input" type="email" placeholder="Correo Electrónico de usuario" id="email" name="email">
-        </div> <!-- /form__field -->        
-        <div class="form__field">
-            <label for="password" class="form__label">Contraseña</label>
-            <input class="form__input" type="password" placeholder="Contraseña" id="password" name="password">
-        </div> <!-- /form__field -->        
-    </form>
+    <div class="imagen-cuenta"></div>
+    <div class="form-cuenta">
+        <form class="form">
+            <div class="form__field">
+                <label for="name" class="form__label">Nombre Usuario</label>
+                <input class="form__input" type="text" placeholder="Nombre de usuario" id="name" name="name">
+            </div> <!-- /form__field -->        
+            <div class="form__field">
+                <label for="phone" class="form__label">Teléfono</label>
+                <input class="form__input" type="tel" placeholder="Teléfono de usuario" id="phone" name="phone">
+            </div> <!-- /form__field -->        
+            <div class="form__field">
+                <label for="email" class="form__label">Correo Electrónico</label>
+                <input class="form__input" type="email" placeholder="Correo Electrónico de usuario" id="email" name="email">
+            </div> <!-- /form__field -->        
+            <div class="form__field">
+                <label for="password" class="form__label">Contraseña</label>
+                <input class="form__input" type="password" placeholder="Contraseña" id="password" name="password">
+            </div> <!-- /form__field -->        
+        </form>
+    </div>
 
 </div> 
 
